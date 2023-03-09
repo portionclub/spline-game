a7e6219106805f24efbd71ca55fe348dfffd5a1e
Vance Ingalls <vanceingalls@gmail.com>
2023-03-08 17:32:39 -0800

    chore: first

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div className='h-screen'>
+      <Spline scene='https://prod.spline.design/ag0pmh3z1vT1Gqtd/scene.splinecode' />
+    </div>
+  );
+}
